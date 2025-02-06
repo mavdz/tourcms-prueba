e465b9018d20d72c37dbd2660f910d903aa39ba9
mavdz <145556981+mavdz@users.noreply.github.com>
2025-02-06 08:57:11 +0100

    Configurar parámetros de búsqueda y hacer call a la API

--- src/main.php
+++ src/main.php
@@ -8,3 +8,14 @@ require_once "TourCMS.php";
 // Crear la instancia de TourCMS usando un namespace
 use TourCMS\Utils\TourCMS as TourCMS;
 $tourcms = new TourCMS($marketplace_id, $api_key, "simplexml");
+
+// Declarar las variables que vamos a utilizar para realizar la búsqueda en la API y para mostrar los resultados de la misma en la web
+$results_html = "";
+$search_term = "";
+
+// Configurar los parámetros de búsqueda para que la API solo busque tours/viajes/actividades/atracciones
+// de un solo día (sin estancia nocturna) y que solo tengan lugar en España
+$params = "product_type=4&country=ES&search=" . urlencode($search_term);
+
+// Hacer la call a la API utilizando la función search_tours
+$result = $tourcms->search_tours($params, $channel_id);
